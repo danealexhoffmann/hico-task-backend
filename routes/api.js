17d39bf4f837d1a7aae29f8e43af14c395770e7c
danealexhoffmann <danehoffmann@gmail.com>
2025-05-02 09:17:55 +0200

    refactor: remove unused data route and clean up employee data retrieval

--- routes/api.js
+++ routes/api.js
@@ -7,17 +7,7 @@ router.get('/test', (req, res) => {
 	res.json({ message: 'API working' });
 });
 
-//Getting data from mysql
-router.get('/data', async (req, res) => {
-	try {
-		const [rows] = await pool.query('SELECT * FROM your_table');
-		res.json({ message: rows });
-	} catch (err) {
-		res.status(500).json({ error: err.message });
-	}
-})
-
-//Task employee data
+//Get employee data
 router.get('/employees', async (req, res) => {
 	try {
 		const [rows] = await pool.query('SELECT * FROM current_employees');
